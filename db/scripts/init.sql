83a41babb8a540e2eea9740cb4c99db3d1e7c478
George Kaye <georgejkaye@gmail.com>
2025-07-22 21:35:21 +0100

    feat: add init db script

---
+++ db/scripts/init.sql
@@ -0,0 +1,23 @@
+CREATE TABLE app_user (
+    user_id SERIAL PRIMARY KEY,
+    user_name TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE venue (
+    venue_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    latitude DECIMAL NOT NULL,
+    longitude DECIMAL NOT NULL
+);
+
+CREATE TABLE visit (
+    visit_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    venue_id INTEGER NOT NULL,
+    visit_date TIMESTAMP WITH TIME ZONE,
+    notes TEXT,
+    rating INTEGER,
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+    FOREIGN KEY (venue_id) REFERENCES venue(venue_id)
+);
