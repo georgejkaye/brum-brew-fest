83be8926f1fc04314424f13261fd3a6e1d652458
George Kaye <georgejkaye@gmail.com>
2025-07-23 22:32:04 +0100

    feat: add api routes

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,14 +1,55 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from psycopg import Connection
 
-from api.utils import get_env_variable
+from api.classes import UserSummary, UserVisit, Venue
+from api.db import (
+    select_user_summary,
+    select_venue_by_venue_id,
+    select_venues,
+    select_visits,
+)
+from api.utils import get_env_variable, get_secret
 
 app = FastAPI(title="Brum Brew Fest Tracker")
+
+conn = Connection.connect(
+    dbname=get_env_variable("DB_NAME"),
+    user=get_env_variable("DB_USER"),
+    password=get_secret("DB_PASSWORD"),
+    host=get_env_variable("DB_HOST"),
+)
 
 
 @app.get("/", summary="Say hello!")
 async def hello() -> str:
     return "Hello!"
+
+
+@app.get("/users/{user_id}", summary="Get a user and their visits")
+async def get_user_by_user_id(user_id: int) -> UserSummary:
+    summary = select_user_summary(conn, user_id)
+    if summary is None:
+        raise HTTPException(status_code=404)
+    return summary
+
+
+@app.get("/venues", summary="Get a list of venues and their visits")
+async def get_venues() -> list[Venue]:
+    return select_venues(conn)
+
+
+@app.get("/venues/{venue_id}", summary="Get a venue and its visits")
+async def get_venue_by_id(venue_id: int) -> Venue:
+    venue = select_venue_by_venue_id(conn, venue_id)
+    if venue is None:
+        raise HTTPException(status_code=404)
+    return venue
+
+
+@app.get("/visits", summary="Get all the visits")
+async def get_visits() -> list[UserVisit]:
+    return select_visits(conn)
 
 
 def start() -> None:
