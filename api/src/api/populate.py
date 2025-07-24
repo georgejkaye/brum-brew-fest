5539fbfed2f172a84eed21f300629541f71798c5
George Kaye <georgejkaye@gmail.com>
2025-07-24 22:27:49 +0100

    feat: add populate venue script

---
+++ api/src/api/populate.py
@@ -0,0 +1,34 @@
+import json
+import sys
+
+from psycopg import Connection
+
+from api.classes import VenueInput
+from api.db import insert_venues
+from api.utils import get_secret_file_contents
+
+venue_json_path = sys.argv[1]
+
+with open(venue_json_path, "r") as f:
+    venue_json = json.load(f)
+
+venues: list[VenueInput] = []
+
+for venue in venue_json:
+    venues.append(
+        VenueInput(
+            venue["name"],
+            venue["address"],
+            venue["latitude"],
+            venue["longitude"],
+        )
+    )
+
+conn = Connection.connect(
+    host=sys.argv[2],
+    dbname=sys.argv[3],
+    user=sys.argv[4],
+    password=get_secret_file_contents(sys.argv[5]),
+)
+
+insert_venues(conn, venues)
