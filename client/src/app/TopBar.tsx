75d907c032040e48dbe888cecf86e7c316a44f04
George Kaye <georgejkaye@gmail.com>
2025-07-26 22:01:51 +0100

    feat: adjust top bar

--- client/src/app/TopBar.tsx
+++ client/src/app/TopBar.tsx
@@ -1,11 +1,17 @@
 import { useContext } from "react"
 import { DataContext } from "./context/data"
+import Link from "next/link"
 
 const TopBar = () => {
     return (
         <div className="flex flex-row p-4 bg-[#38db98]">
-            <div className="text-2xl text-black font-bold">
+            <div className="flex-1 text-2xl text-black font-bold">
                 Brum Brew Fest Tracker
+            </div>
+            <div className="flex flex-row gap-4">
+                <Link href="/">Map</Link>
+                <Link href="/venues">Venues</Link>
+                <Link href="/login">Login</Link>
             </div>
         </div>
     )
