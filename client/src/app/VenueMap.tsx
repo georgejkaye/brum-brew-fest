4633e1b932307052d44a5c3d00691e3c809bad09
George Kaye <georgejkaye@gmail.com>
2025-07-26 17:37:13 +0100

    feat: add map to map page

--- client/src/app/VenueMap.tsx
+++ client/src/app/VenueMap.tsx
@@ -1,17 +1,26 @@
-import { Map } from "@vis.gl/react-maplibre";
-import "maplibre-gl/dist/maplibre-gl.css";
-import mapJson from "./map.json";
+"use client"
+import { Map } from "@vis.gl/react-maplibre"
+import "maplibre-gl/dist/maplibre-gl.css"
+import { useContext, useEffect } from "react"
+import { DataContext } from "./context/data"
 
-export const VenueMap = () => {
-  return (
-    <Map
-      initialViewState={{
-        latitude: 52.4864,
-        longitude: -1.9422,
-        zoom: 12.5,
-      }}
-      style={{ width: "100%", height: "100vh" }}
-      mapStyle={"https://tiles.openfreemap.org/styles/bright"}
-    />
-  );
-};
+interface VenueMapProps {
+    venues: Venue[]
+}
+
+export const VenueMap = ({ venues }: VenueMapProps) => {
+    useEffect(() => {
+        console.log(venues)
+    }, [venues])
+    return (
+        <Map
+            initialViewState={{
+                latitude: 52.4864,
+                longitude: -1.9422,
+                zoom: 12.5,
+            }}
+            style={{ width: "100%", height: "100vh" }}
+            mapStyle={"https://tiles.openfreemap.org/styles/bright"}
+        />
+    )
+}
