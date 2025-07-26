36249e2dbcb17ac1b875fc95f3ed41fadb5ecc8e
George Kaye <georgejkaye@gmail.com>
2025-07-26 17:36:26 +0100

    style: apply prettier to top bar

--- client/src/app/TopBar.tsx
+++ client/src/app/TopBar.tsx
@@ -1,9 +1,9 @@
 const TopBar = () => {
-  return (
-    <div className="flex flex-row p-4 bg-[#38db98]">
-      <div className="text-black font-bold">Brum Brew Fest Tracker</div>
-    </div>
-  );
-};
+    return (
+        <div className="flex flex-row p-4 bg-[#38db98]">
+            <div className="text-black font-bold">Brum Brew Fest Tracker</div>
+        </div>
+    )
+}
 
-export default TopBar;
+export default TopBar
