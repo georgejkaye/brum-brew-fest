48911d50211dee0f6b6741071d1661137c68d4ff
George Kaye <georgejkaye@gmail.com>
2025-07-26 21:59:42 +0100

    feat: add axios calls for auth

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -1,5 +1,103 @@
 import axios from "axios"
-import { Venue, User, Visit } from "./interfaces"
+import { Venue, User, Visit, UserSummary } from "./interfaces"
+
+const responseToUser = (response: any) => ({
+    userId: response["id"],
+    displayName: response["display_name"],
+    email: response["email"],
+    isVerified: response["is_verified"],
+})
+
+export const login = async (email: string, password: string) => {
+    let endpoint = "/auth/jwt/login"
+    try {
+        let body = {
+            grant_type: "password",
+            username: email,
+            password: password,
+        }
+        let headers = {
+            accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        let response = await axios.post(endpoint, body, { headers })
+        let data = response.data
+        return data["access_token"]
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
+
+export const registerUser = async (
+    email: string,
+    password: string,
+    displayName: string
+) => {
+    let endpoint = "/api/auth/register"
+    try {
+        let body = {
+            email: email,
+            password: password,
+            display_name: displayName,
+        }
+        let response = await axios.post(endpoint, body)
+        let data = response.data
+        let user = responseToUser(data)
+        return user
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
+
+export const requestVerifyToken = async (email: string) => {
+    let endpoint = "/api/auth/request-verify-token"
+    try {
+        let body = { email }
+        await axios.post(endpoint, body)
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
+
+export const verifyUser = async (token: string) => {
+    let endpoint = "/api/auth/verify"
+    try {
+        let body = { token }
+        let response = await axios.post(endpoint, body)
+        let data = response.data
+        let user = responseToUser(data)
+        return user
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
+
+export const requestPasswordReset = async (email: string) => {
+    let endpoint = "/api/auth/forgot-password"
+    try {
+        let body = { email }
+        await axios.post(endpoint, body)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+export const resetPassword = async (token: string) => {
+    let endpoint = "/api/auth/verify"
+    try {
+        let body = { token }
+        let response = await axios.post(endpoint, body)
+        let data = response.data
+        return data as string
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
 
 const responseToVenueVisit = (response: any) => ({
     visitId: response["visit_id"],
