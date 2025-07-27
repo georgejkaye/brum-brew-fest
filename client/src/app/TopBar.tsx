91f02689640d72544fe4ce3ae1e421f21db26c03
George Kaye <georgejkaye@gmail.com>
2025-07-27 11:22:07 +0100

    refactor: move user retrieval into top bar

--- client/src/app/TopBar.tsx
+++ client/src/app/TopBar.tsx
@@ -1,11 +1,25 @@
+"use client"
 import Link from "next/link"
 import { User } from "./interfaces"
+import { useState, useEffect } from "react"
+import { getUserDetails } from "./api"
 
-interface TopBarProps {
-    user: User | undefined
-}
+const TopBar = () => {
+    const [user, setUser] = useState<User | undefined>(undefined)
+    useEffect(() => {
+        const fetchUser = async (token: string) => {
+            let user = await getUserDetails(token)
+            if (user) {
+                console.log(user)
+                setUser(user)
+            }
+        }
+        let token = localStorage.getItem("token")
+        if (token) {
+            fetchUser(token)
+        }
+    }, [])
 
-const TopBar = ({ user }: TopBarProps) => {
     return (
         <div className="flex flex-row p-4 bg-[#38db98]">
             <div className="flex-1 text-2xl text-black font-bold">
