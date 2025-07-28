927d890d9af7d247807e74a571b05a513ddebc49
George Kaye <georgejkaye@gmail.com>
2025-07-28 22:57:22 +0100

    feat: add menu for small screens

--- client/src/app/TopBar.tsx
+++ client/src/app/TopBar.tsx
@@ -1,23 +1,60 @@
 "use client"
 import Link from "next/link"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "./context/user"
 
 const TopBar = () => {
     const { user, isLoadingUser } = useContext(UserContext)
+    const [isMenuOpen, setMenuOpen] = useState(false)
+    const onClickLink = () => {
+        setMenuOpen(false)
+    }
     return (
-        <div className="flex flex-row p-4 bg-[#38db98] w-full">
-            <div className="flex-1 text-2xl text-black font-bold">
-                <Link href="/">Brum Brew Fest Tracker</Link>
+        <div>
+            <div className="flex flex-row p-4 bg-[#38db98] w-full items-center">
+                <div className="flex-1 text-2xl text-black font-bold">
+                    <Link href="/">Brum Brew Fest Tracker</Link>
+                </div>
+                {!isLoadingUser && (
+                    <>
+                        <div className="hidden md:flex flex-row gap-4">
+                            <Link href="/">Map</Link>
+                            <Link href="/venues/list">Venues</Link>
+                            {user ? (
+                                <Link href="/users/account">
+                                    {user.displayName}
+                                </Link>
+                            ) : (
+                                <Link href="/login">Login</Link>
+                            )}
+                        </div>
+                        <div
+                            onClick={(e) =>
+                                setMenuOpen((isMenuOpen) => !isMenuOpen)
+                            }
+                            className="md:hidden cursor-pointer"
+                        >
+                            Menu
+                        </div>
+                    </>
+                )}
             </div>
-            {!isLoadingUser && (
-                <div className="flex flex-row gap-4">
-                    <Link href="/">Map</Link>
-                    <Link href="/venues/list">Venues</Link>
+            {isMenuOpen && (
+                <div className="absolute z-999 top-16 left-0 bg-green-300 p-4 flex flex-col gap-3 items-end w-full">
+                    <Link onClick={onClickLink} href="/">
+                        Map
+                    </Link>
+                    <Link onClick={onClickLink} href="/venues/list">
+                        Venues
+                    </Link>
                     {user ? (
-                        <Link href="/users/account">{user.displayName}</Link>
+                        <Link onClick={onClickLink} href="/users/account">
+                            {user.displayName}
+                        </Link>
                     ) : (
-                        <Link href="/login">Login</Link>
+                        <Link onClick={onClickLink} href="/login">
+                            Login
+                        </Link>
                     )}
                 </div>
             )}
