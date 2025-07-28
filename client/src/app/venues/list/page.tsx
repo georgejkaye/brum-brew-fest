7e37c0944fb09866a17f04ed8be5c077789f6bb5
George Kaye <georgejkaye@gmail.com>
2025-07-28 22:20:34 +0100

    feat: polish up venue list

--- client/src/app/venues/list/page.tsx
+++ client/src/app/venues/list/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Venue } from "../../interfaces"
 import { VenuesContext } from "@/app/context/venues"
 import { Rating } from "@smastrom/react-rating"
@@ -12,36 +12,54 @@ interface VenueCardProps {
 const VenueCard = ({ venue }: VenueCardProps) => {
     let visitCount = venue.visits.length
     let venueAverageRating =
-        venue.visits.reduce((a, b) => a + b.rating, 0) / visitCount
-    let venueAverageRatingValue = venueAverageRating ? venueAverageRating : 0
+        visitCount === 0
+            ? 0
+            : venue.visits.reduce((a, b) => a + b.rating, 0) / visitCount
     return (
         <Link href={`/venues/${venue.venueId}`}>
-            <div className="p-4 flex md:flex-row items-end gap-4 bg-green-100 rounded-lg shadow">
-                <div className="flex flex-col flex-1">
+            <div className="p-4 flex md:flex-row items-end gap-4 bg-green-100 rounded-lg shadow hover:bg-green-200">
+                <div className="flex flex-col flex-1 gap-2">
                     <div className="text-2xl font-bold">{venue.name}</div>
                     <div>{venue.address}</div>
-                    <div className="flex flex-row w-full">
-                        {venueAverageRatingValue} out of 5
-                    </div>
-                </div>
-                <div className="flex flex-col">
                     <div>{visitCount} visits</div>
-                    <div className=""></div>
+                    <Rating
+                        style={{ maxWidth: 100 }}
+                        value={venueAverageRating}
+                        readOnly
+                    />
                 </div>
             </div>
         </Link>
     )
 }
 
-interface VenueListProps {
-    venues: Venue[]
-}
-
 export const Page = () => {
     const { venues } = useContext(VenuesContext)
+    const [filteredVenues, setFilteredVenues] = useState([...venues])
+    const [searchValue, setSearchValue] = useState("")
+    useEffect(() => {
+        setFilteredVenues(
+            venues.filter(
+                (venue) =>
+                    venue.name
+                        .toLowerCase()
+                        .includes(searchValue.toLowerCase()) ||
+                    venue.address
+                        .toLowerCase()
+                        .includes(searchValue.toLowerCase())
+            )
+        )
+    }, [searchValue])
     return (
         <div className="flex flex-col gap-4 lg:w-1/3 mx-auto p-4">
-            {venues.map((venue) => (
+            <input
+                type="text"
+                placeholder="Type to filter..."
+                className="p-4 rounded-xl border-gray-400 border-1"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
+            {filteredVenues.map((venue) => (
                 <VenueCard key={venue.venueId} venue={venue} />
             ))}
         </div>
