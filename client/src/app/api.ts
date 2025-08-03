6d7844f2f836a267b1f1e03795b4353ad47ae604
George Kaye <georgejkaye@gmail.com>
2025-08-03 20:16:41 +0100

    fix: update client api call to get areas

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -155,8 +155,8 @@ const responseToVenue = (response: any) => ({
     longitude: response["longitude"],
     visits: response["visits"].map(responseToVenueVisit),
     pinLocation: response["pin_location"],
-    venueAreaId: response["venue_area_id"],
-    venueAreaName: response["venue_area_name"],
+    venueAreaId: response["area_id"],
+    venueAreaName: response["area_name"],
 })
 
 export const getVenues = async (): Promise<Venue[]> => {
