b04dcfa4e891795b097d4ec27cbc5b311539686c
George Kaye <georgejkaye@gmail.com>
2025-08-04 21:32:57 +0100

    chore: remove used script

--- db/updates/16-venue-update-script.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE area ADD CONSTRAINT area_unique_area_name UNIQUE (area_name);
-
-ALTER TABLE venue
-ADD CONSTRAINT venue_unique_venue_name_venue_address
-UNIQUE (venue_name, venue_address);
