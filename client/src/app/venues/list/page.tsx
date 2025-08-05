9d6b946447b347bb1324157db2f0509e71720380
Matthew Hammond <69700997+FrozenDude101@users.noreply.github.com>
2025-08-05 21:45:23 +0100

    feat: change direction

--- client/src/app/venues/list/page.tsx
+++ client/src/app/venues/list/page.tsx
@@ -19,7 +19,7 @@ const VenueCard = ({ venue }: VenueCardProps) => {
         <Link href={`/venues/${venue.venueId}`}>
             <div className="p-4 flex md:flex-row items-end gap-4 bg-green-100 rounded-lg shadow hover:bg-green-200">
                 <div className="flex flex-col flex-1 gap-2">
-                    <div className="text-2xl font-bold">{venue.pinLocation ? "🔰 " : ""}{venue.name}</div>
+                    <div className="text-2xl font-bold">{venue.name}{venue.pinLocation ? " 🔰" : ""}</div>
                     <div>{venue.address}</div>
                     <div>{visitCount} visits</div>
                     <Rating
