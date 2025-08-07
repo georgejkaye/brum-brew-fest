7247c759394fbf605ff3e9066e10a111127e05fe
George Kaye <georgejkaye@gmail.com>
2025-08-07 23:03:41 +0100

    feat: continue to develop follows page

--- client/src/app/follows/page.tsx
+++ client/src/app/follows/page.tsx
@@ -1,59 +1,130 @@
 "use client"
-import { useContext, useEffect, useState } from "react"
+import { MouseEvent, useCallback, useContext, useEffect, useState } from "react"
 import { UserFollow } from "../interfaces"
-import { getFollows } from "../api"
+import { getFollows, removeFollow } from "../api"
 import { UserContext } from "../context/user"
 import { useRouter } from "next/navigation"
 import { Loader } from "../Loader"
 import Link from "next/link"
+import { LoginButton } from "../components/login"
 
 interface FollowCardProps {
+    token: string
     follow: UserFollow
+    isEditingFollowers: boolean
+    fetchFollows: () => Promise<void>
 }
 
-const FollowCard = ({ follow }: FollowCardProps) => (
-    <div className="flex flex-col md:flex-row p-4 rounded bg-green-300 gap-4">
-        <Link
-            className="font-bold mr-auto hover:underline"
-            href={`/users/${follow.userId}`}
-        >
-            {follow.displayName}
-        </Link>
-        <div>{follow.visitCount} visits</div>
-        <div>{follow.uniqueVisitCount} venues</div>
-    </div>
-)
+const FollowCard = ({
+    token,
+    follow,
+    isEditingFollowers,
+    fetchFollows,
+}: FollowCardProps) => {
+    const [isLoading, setLoading] = useState(false)
+    const onClickRemoveButton = async (e: MouseEvent<HTMLButtonElement>) => {
+        setLoading(true)
+        await removeFollow(token, follow.userId)
+        await fetchFollows()
+    }
+    return (
+        <div className="flex items-center w-full">
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div className="flex flex-col md:flex-row p-4 rounded bg-green-200 gap-4 w-full">
+                    <Link
+                        className="font-bold mr-auto hover:underline"
+                        href={`/users/${follow.userId}`}
+                    >
+                        {follow.displayName}
+                    </Link>
+                    {isEditingFollowers ? (
+                        <button
+                            className="hover:underline cursor-pointer font-bold"
+                            onClick={onClickRemoveButton}
+                        >
+                            Remove
+                        </button>
+                    ) : (
+                        <div className="flex flex-row gap-4">
+                            <div>{follow.visitCount} visits</div>
+                            <div>{follow.uniqueVisitCount} venues</div>
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    )
+}
 
 const Page = () => {
     const router = useRouter()
     const { token } = useContext(UserContext)
 
-    const [isLoading, setLoading] = useState(true)
     const [follows, setFollows] = useState<UserFollow[]>([])
+    const [users, setUsers] = useState<UserFollow[]>([])
+
+    const [isLoadingFollows, setLoadingFollows] = useState(true)
+    const [isLoadingUsers, setLoadingUsers] = useState(true)
+    const [isAddingFollower, setAddingFollower] = useState(false)
+    const [isEditingFollowers, setEditingFollowers] = useState(false)
+
+    const fetchFollows = useCallback(async (token: string) => {
+        const follows = await getFollows(token)
+        setFollows(follows)
+        setLoadingFollows(false)
+    }, [])
 
     useEffect(() => {
-        const fetchFollows = async (token: string) => {
-            const follows = await getFollows(token)
-            setFollows(follows)
-            setLoading(false)
+        const fetchUsers = async () => {
+            const users: UserFollow[] = []
+            setUsers(users)
+            setLoadingUsers(false)
         }
         if (!token) {
             router.push("/")
         } else {
             fetchFollows(token)
+            fetchUsers()
         }
-    }, [router, token])
+    }, [router, token, fetchFollows])
+
+    const onClickAddFollowerButton = (e: MouseEvent<HTMLButtonElement>) => {}
+    const onClickEditFollowersButton = (e: MouseEvent<HTMLButtonElement>) => {
+        setEditingFollowers((old) => !old)
+    }
 
     return (
         <div className="flex flex-col md:mx-auto md:w-1/2 lg:w-1/3 p-4 items-center">
-            {isLoading ? (
+            {!token || isLoadingFollows ? (
                 <Loader />
             ) : (
                 <div className="flex flex-col gap-4 w-full">
                     <h2 className="font-bold text-2xl">Follows</h2>
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-row gap-4">
+                        <LoginButton
+                            label="Add followers"
+                            onClick={onClickAddFollowerButton}
+                        />
+                        <LoginButton
+                            label={
+                                isEditingFollowers
+                                    ? "Stop editing followers"
+                                    : "Edit followers"
+                            }
+                            onClick={onClickEditFollowersButton}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-4 w-full">
                         {follows.map((follow) => (
-                            <FollowCard key={follow.userId} follow={follow} />
+                            <FollowCard
+                                key={follow.userId}
+                                follow={follow}
+                                isEditingFollowers={isEditingFollowers}
+                                token={token}
+                                fetchFollows={() => fetchFollows(token)}
+                            />
                         ))}
                     </div>
                 </div>
