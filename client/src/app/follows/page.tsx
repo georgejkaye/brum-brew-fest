9f9c31054c1375a740f69575ccaadf3e4d3d9049
George Kaye <georgejkaye@gmail.com>
2025-08-08 23:20:20 +0100

    feat: add follower feature

--- client/src/app/follows/page.tsx
+++ client/src/app/follows/page.tsx
@@ -1,5 +1,12 @@
 "use client"
-import { MouseEvent, useCallback, useContext, useEffect, useState } from "react"
+import {
+    ChangeEvent,
+    MouseEvent,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from "react"
 import { UserCount, UserFollow } from "../interfaces"
 import { addFollow, getFollows, getUserCounts, removeFollow } from "../api"
 import { UserContext } from "../context/user"
@@ -28,7 +35,7 @@ const FollowCard = ({ token, follow, fetchFollows }: FollowCardProps) => {
             ) : (
                 <div className="flex flex-col md:flex-row p-4 rounded bg-green-200 gap-4 w-full">
                     <Link
-                        className="font-bold mr-auto hover:underline"
+                        className="font-bold md:mr-auto hover:underline"
                         href={`/users/${follow.userId}`}
                     >
                         {follow.displayName}
@@ -38,7 +45,7 @@ const FollowCard = ({ token, follow, fetchFollows }: FollowCardProps) => {
                         <div>{follow.uniqueVisitCount} venues</div>
                     </div>
                     <button
-                        className="hover:underline cursor-pointer font-bold"
+                        className="hover:underline cursor-pointer font-bold text-left"
                         onClick={onClickRemoveButton}
                     >
                         Remove
@@ -62,25 +69,36 @@ const AddFollowCard = ({
     follows,
     fetchFollows,
 }: AddFollowCardProps) => {
+    const following = follows.some((follow) => follow.userId === user.userId)
+    console.log(following)
     const [isLoading, setLoading] = useState(false)
-    const onClickAddButton = async (e: MouseEvent<HTMLButtonElement>) => {
+    const performAddFollow = async () => {
         setLoading(true)
-        await addFollow(token, user.userId)
-        await fetchFollows()
+        if (!follows.some((follow) => follow.userId === user.userId)) {
+            await addFollow(token, user.userId)
+            await fetchFollows()
+        }
         setLoading(false)
     }
+    const onClickAddButton = (e: MouseEvent<HTMLButtonElement>) => {
+        performAddFollow()
+    }
+    const onClickCard = (e: MouseEvent<HTMLDivElement>) => {
+        performAddFollow()
+    }
+    const cardStyle =
+        "flex flex-col md:flex-row p-4 rounded bg-green-200 gap-4 w-full"
+    const hoverStyle = "hover:bg-green-100 cursor-pointer"
     return (
         <div className="flex flex-row items-center w-full">
             {isLoading ? (
                 <Loader size={50} />
             ) : (
-                <div className="flex flex-col md:flex-row p-4 rounded bg-green-200 gap-4 w-full">
-                    <Link
-                        className="font-bold mr-auto hover:underline"
-                        href={`/users/${user.userId}`}
-                    >
-                        {user.displayName}
-                    </Link>
+                <div
+                    className={`${cardStyle} ${following ? "" : hoverStyle}`}
+                    onClick={onClickCard}
+                >
+                    <div className="font-bold mr-auto">{user.displayName}</div>
                     <div className="flex flex-row gap-4">
                         <div>{user.visitCount} visits</div>
                         <div>{user.uniqueVisitCount} venues</div>
@@ -103,19 +121,33 @@ const AddFollowCard = ({
     )
 }
 const Page = () => {
+    const nameAscendingSort = (a: UserCount, b: UserCount) =>
+        a.displayName.localeCompare(b.displayName)
+    const nameDescendingSort = (a: UserCount, b: UserCount) =>
+        b.displayName.localeCompare(a.displayName)
+    const visitsAscendingSort = (a: UserCount, b: UserCount) =>
+        a.visitCount - b.visitCount
+    const visitsDescendingSort = (a: UserCount, b: UserCount) =>
+        b.visitCount - a.visitCount
+    const venuesAscendingSort = (a: UserCount, b: UserCount) =>
+        a.uniqueVisitCount - b.uniqueVisitCount
+    const venuesDescendingSort = (a: UserCount, b: UserCount) =>
+        b.uniqueVisitCount - a.uniqueVisitCount
+
     const router = useRouter()
     const { isLoadingUser, token, user } = useContext(UserContext)
 
     const [follows, setFollows] = useState<UserFollow[]>([])
     const [users, setUsers] = useState<UserCount[]>([])
+    const [filteredFollows, setFilteredFollows] = useState<UserFollow[]>([])
     const [filteredUsers, setFilteredUsers] = useState<UserCount[]>([])
 
     const [isLoadingFollows, setLoadingFollows] = useState(true)
     const [isLoadingUsers, setLoadingUsers] = useState(true)
     const [isAddingFollower, setAddingFollower] = useState(false)
-    const [isEditingFollowers, setEditingFollowers] = useState(false)
 
     const [userSearchText, setUserSearchText] = useState("")
+    const [sortByValue, setSortByValue] = useState("name-asc")
 
     const fetchFollows = useCallback(async (token: string) => {
         const follows = await getFollows(token)
@@ -143,22 +175,37 @@ const Page = () => {
     }, [router, token, fetchFollows, isLoadingUser, user])
 
     useEffect(() => {
-        setFilteredUsers(
-            users.filter(
-                (user) =>
-                    userSearchText === "" ||
-                    user.displayName
+        const getSortByFunction = () =>
+            sortByValue === "name-asc"
+                ? nameAscendingSort
+                : sortByValue === "name-desc"
+                ? nameDescendingSort
+                : sortByValue === "visits-asc"
+                ? visitsAscendingSort
+                : sortByValue === "visits-desc"
+                ? visitsDescendingSort
+                : sortByValue === "venues-asc"
+                ? venuesAscendingSort
+                : venuesDescendingSort
+        const filterAndSortUsers = (userArray: UserCount[]) =>
+            userArray
+                .filter((userCount) =>
+                    userCount.displayName
                         .toLowerCase()
                         .includes(userSearchText.toLowerCase())
-            )
-        )
-    }, [userSearchText, users])
+                )
+                .sort(getSortByFunction())
+        const filterAndSortFollows = (followArray: UserFollow[]) =>
+            followArray.sort(getSortByFunction())
+        setFilteredUsers(filterAndSortUsers(users))
+        setFilteredFollows(filterAndSortFollows(follows))
+    }, [userSearchText, users, sortByValue, follows])
 
     const onClickAddFollowerButton = (e: MouseEvent<HTMLButtonElement>) => {
         setAddingFollower((old) => !old)
     }
-    const onClickEditFollowersButton = (e: MouseEvent<HTMLButtonElement>) => {
-        setEditingFollowers((old) => !old)
+    const onChangeSortBy = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSortByValue(e.target.value)
     }
 
     return (
@@ -168,7 +215,7 @@ const Page = () => {
             ) : (
                 <div className="flex flex-col gap-4 w-full">
                     <h2 className="font-bold text-2xl">Follows</h2>
-                    <div className="flex flex-row gap-4">
+                    <div className="flex flex-col lg:flex-row gap-4 items-start lg:items-center">
                         <LoginButton
                             label={
                                 isAddingFollower
@@ -177,6 +224,32 @@ const Page = () => {
                             }
                             onClick={onClickAddFollowerButton}
                         />
+                        <div className="flex flex-row gap-2 items-center">
+                            <label className="ml-auto" htmlFor="sort-by">
+                                Sort by
+                            </label>
+                            <select
+                                className="border-1 rounded p-2"
+                                name="sort-by"
+                                value={sortByValue}
+                                onChange={onChangeSortBy}
+                            >
+                                <option value="name-asc">A-Z</option>
+                                <option value="name-desc">Z-A</option>
+                                <option value="visits-desc">
+                                    Visits (high-low)
+                                </option>
+                                <option value="visits-asc">
+                                    Visits (low-high)
+                                </option>
+                                <option value="venues-desc">
+                                    Venues (high-low)
+                                </option>
+                                <option value="rating-asc">
+                                    Venues (low-high)
+                                </option>
+                            </select>
+                        </div>
                     </div>
                     {isAddingFollower ? (
                         <div className="flex flex-col gap-4">
@@ -186,26 +259,32 @@ const Page = () => {
                                 setValue={setUserSearchText}
                                 type="text"
                                 onKeyDown={() => {}}
+                                placeholder="Type to filter users"
                             />
-                            <div className="flex flex-col gap-4">
-                                {filteredUsers.map((user) => (
-                                    <AddFollowCard
-                                        key={user.userId}
-                                        user={user}
-                                        follows={follows}
-                                        token={token}
-                                        fetchFollows={() => fetchFollows(token)}
-                                    />
-                                ))}
-                            </div>
+                            {isLoadingUsers ? (
+                                <Loader />
+                            ) : (
+                                <div className="flex flex-col gap-4">
+                                    {filteredUsers.map((user) => (
+                                        <AddFollowCard
+                                            key={user.userId}
+                                            user={user}
+                                            follows={follows}
+                                            token={token}
+                                            fetchFollows={() =>
+                                                fetchFollows(token)
+                                            }
+                                        />
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     ) : (
                         <div className="flex flex-col gap-4 w-full">
-                            {follows.map((follow) => (
+                            {filteredFollows.map((follow) => (
                                 <FollowCard
                                     key={follow.userId}
                                     follow={follow}
-                                    isEditingFollowers={isEditingFollowers}
                                     token={token}
                                     fetchFollows={() => fetchFollows(token)}
                                 />
